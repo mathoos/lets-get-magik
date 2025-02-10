df2bcf15c70a94aaf949b43d8e9c58d590bc3e79
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2025-02-10 15:19:53 +0100

    create next app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-500">
+            Bienvenue sur mon e-commerce 🚀
+        </h1>
+    </div>
+  );
+}
---
+++ src/app/produits/page.tsx
@@ -0,0 +1,9 @@
+export default function ProduitsPage() {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Nos Produits</h1>
+        <p>Bientôt disponible...</p>
+      </div>
+    );
+  }
+  
