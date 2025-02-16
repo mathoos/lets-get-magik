02cbe891e241f19adb27a148fa6291c03c206557
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2025-02-16 12:15:30 +0100

    page.tsx filtre catégories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ type Produit = {
 export default function Home() {
     const [produits, setProduits] = useState<Produit[]>([]);
     const [categorieFiltre, setCategorieFiltre] = useState<string | null>(null);
+    const [categories, setCategories] = useState<string[]>([]);
     const { ajouterAuPanier } = useCart();
     const { recherche } = useSearch();
 
@@ -31,6 +32,13 @@ export default function Home() {
             } 
             else {
                 setProduits(data);
+
+                const allCategories = ["Soin", "Sérum", "Crème"];
+                const uniqueCategories = Array.from(new Set(data.map((prod) => prod.categorie).filter(cat => cat))); 
+
+                setCategories(Array.from(new Set([...allCategories, ...uniqueCategories])));
+
+
             }
         }
         fetchProduits();
@@ -55,58 +63,45 @@ export default function Home() {
 
                 {/*Filtres de catégorie */}
                 <div className="home_search-tags">
-                    
-                
                     <button 
                         onClick={() => setCategorieFiltre(null)}
                         className={`tag ${categorieFiltre === null ? "active" : ""}`}
                     >
                         Tous
                     </button>
-                    <button 
-                        onClick={() => setCategorieFiltre("Maquillage")}
-                        className={`tag ${categorieFiltre === "Maquillage" ? "active" : ""}`}
-                    >
-                        Crèmes
-                    </button>
-                    <button 
-                        onClick={() => setCategorieFiltre("Soin")}
-                        className={`tag ${categorieFiltre === "Soin" ? "active" : ""}`}
-                    >
-                        Soins
-                    </button>
-                    <button 
-                        onClick={() => setCategorieFiltre("Soin")}
-                        className={`tag ${categorieFiltre === "Soin" ? "active" : ""}`}
-                    >
-                        Sérums
-                    </button>
-                 
+                    {categories.map((cat) => (
+                        <button 
+                            key={cat}
+                            onClick={() => setCategorieFiltre(cat)}
+                            className={`tag ${categorieFiltre === cat ? "active" : ""}`}
+                        >
+                            {cat}
+                        </button>
+                    ))}
                 </div>
-
             </div>
 
             {/*Liste des produits */}
             <div className="home_produits">
                 {produitsFiltres.length === 0 ? (
-                <p>Aucun produit trouvé</p>
+                    <p>Aucun produit trouvé</p>
                 ) : (
-                <div className="home_produits-container">
-                    {produitsFiltres.map((produit) => (
-                        <div key={produit.id} className="produit">
-                            <Link href={`/produit/${produit.id}`} className="produit_link">
-                                <h2>{produit.nom}</h2>
-                                <img src={produit.image} alt={produit.nom} className="produit_link-img"/>
-                            </Link>
-                            <div className="produit_info">
-                                <p>{produit.prix} €</p>
-                                <button onClick={() => ajouterAuPanier({ ...produit, quantite: 1 })}>
-                                    Ajouter au panier
-                                </button>
+                    <div className="home_produits-container">
+                        {produitsFiltres.map((produit) => (
+                            <div key={produit.id} className="produit">
+                                <Link href={`/produit/${produit.id}`} className="produit_link">
+                                    <h2>{produit.nom}</h2>
+                                    <img src={produit.image} alt={produit.nom} className="produit_link-img"/>
+                                </Link>
+                                <div className="produit_info">
+                                    <p>{produit.prix} €</p>
+                                    <button onClick={() => ajouterAuPanier({ ...produit, quantite: 1 })}>
+                                        Ajouter au panier
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
                 )}
             </div>
         </div>
