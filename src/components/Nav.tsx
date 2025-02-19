fbc069ba00a2c8a0f227858f4a9331591d5489f9
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2025-02-19 09:33:47 +0100

    navbar tailwind

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import Panier from "./Panier"; 
 import { useCart } from "../context/CartContext";
 import { useSearch } from "../context/SearchContext"; 
 
-import "./Nav.scss";
+import Link from "next/link";
+import Image from "next/image";
+import Panier from "./Panier"; 
 
 import heart from "../assets/heart.svg";
 import cart from "../assets/cart.svg";
@@ -19,41 +18,41 @@ export default function Nav() {
 
     return (
         <>
-        <nav className="nav">
-            <div className="nav_links">
-                <Link href="/" className="nav_links-link">
-                    Home
-                </Link>
-                <Link href="/diagnostic" className="nav_links-link">
-                    Diagnostic
-                </Link>
-                <Link href="/a-propos" className="nav_links-link">
-                    A propos
-                </Link>
-            </div>
-            <div className="nav_links">
-                <div className="nav_links-search">
-                    <input
-                        type="text"
-                        placeholder="Rechercher un produit..." 
-                        value={recherche}
-                        onChange={(e) => setRecherche(e.target.value)} 
-                    />
-                    <Image src={search} alt="Barre de recherche" className="nav_links-icon search"/>
-                </div>
-                <Link href="/" className="">
-                    <Image src={heart} alt="Favoris" className="nav_links-icon"/>
-                </Link>
-                <button className="nav_links-cart" onClick={() => setPanierOpen(!panierOpen)}>
-                    <Image src={cart} alt="Panier" className="nav_links-icon"/> 
-                    <div className="nav_links-cart--length">
-                        <p>{panier.length}</p>
-                    </div>
-                </button>
-            </div>
-        </nav>
+            <nav className="fixed top-0 left-0 flex justify-between w-full h-[8vh] px-[5vw] bg-[var(--primary)] z-10 items-center">
 
-        <Panier isOpen={panierOpen} closePanier={() => setPanierOpen(false)} />
-    </>
+                <div className="flex items-center gap-[2vw]">
+                    <Link href="/" className="text-[var(--secondary)]">Home</Link>
+                    <Link href="/diagnostic" className="text-[var(--secondary)]">Diagnostic</Link>
+                    <Link href="/a-propos" className="text-[var(--secondary)]">A propos</Link>
+                </div>
+
+                <div className="flex items-center gap-[2vw]">
+
+                    <div className="relative w-[20vw] rounded-full bg-[var(--secondary)] flex items-center">
+                        <input
+                            type="text"
+                            placeholder="Rechercher un produit..." 
+                            value={recherche}
+                            onChange={(e) => setRecherche(e.target.value)} 
+                            className="w-full h-full px-5 py-2 outline-none bg-transparent rounded-full text-[var(--primary)]"
+                        />
+                        <Image src={search} alt="Barre de recherche" className="absolute right-3 top-1/2 transform -translate-y-1/2 w-[25px] h-[25px]" />
+                    </div>
+
+                    <Link href="/">
+                        <Image src={heart} alt="Favoris" className="w-[25px] h-[25px]" />
+                    </Link>
+
+                    <button className="relative flex" onClick={() => setPanierOpen(!panierOpen)}>
+                        <Image src={cart} alt="Panier" className="w-[25px] h-[25px]" />
+                        <div className="absolute right-[-17px] bottom-[-5px] flex justify-center items-center w-[20px] h-[20px] bg-[var(--secondary)] rounded-full">
+                            <p className="text-[var(--primary)] text-sm">{panier.length}</p>
+                        </div>
+                    </button>
+                </div>
+            </nav>
+
+            <Panier isOpen={panierOpen} closePanier={() => setPanierOpen(false)} />
+        </>
     );
 }
