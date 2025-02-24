153856c5ef7c7bf70c737a0294b31e5ab202efdd
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2025-02-24 09:44:09 +0100

    layout.tsx preconnect font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,22 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="fr">
+            <head>
+
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Achetez vos produits préférés en ligne" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Gilda+Display&display=swap" rel="stylesheet" />
+            </head>
             <body className="flex flex-col min-h-screen">
                 <CartProvider>
                     <SearchProvider>
-                        <ClientLayout>{children}</ClientLayout>
+                        <ClientLayout>
+                            {children}
+                        </ClientLayout>
                     </SearchProvider>
                 </CartProvider>
             </body>
