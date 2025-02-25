fc5e381adf9fea3ff1e552328c07dee7c3f6da2d
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2025-02-25 14:52:27 +0100

    page.tsx tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import supabase from "@/lib/supabase";
-import { useCart } from "../context/CartContext"; 
-import { useSearch } from "../context/SearchContext"; 
+import { useCart } from "../context/CartContext";
+import { useSearch } from "../context/SearchContext";
 import Header from "../components/Header";
-
-import "./page.scss";
 
 type Produit = {
     id: string;
@@ -14,7 +12,7 @@ type Produit = {
     description: string;
     prix: number;
     image: string;
-    categorie: string; 
+    categorie: string;
 };
 
 export default function Home() {
@@ -29,16 +27,11 @@ export default function Home() {
             const { data, error } = await supabase.from("produits").select("*");
             if (error) {
                 console.error(error);
-            } 
-            else {
+            } else {
                 setProduits(data);
-
                 const allCategories = ["Soin", "Sérum", "Crème"];
-                const uniqueCategories = Array.from(new Set(data.map((prod) => prod.categorie).filter(cat => cat))); 
-
+                const uniqueCategories = Array.from(new Set(data.map((prod) => prod.categorie).filter(cat => cat)));
                 setCategories(Array.from(new Set([...allCategories, ...uniqueCategories])));
-
-
             }
         }
         fetchProduits();
@@ -46,26 +39,25 @@ export default function Home() {
 
     const produitsFiltres = produits.filter(
         (produit) =>
-        (!categorieFiltre || produit.categorie === categorieFiltre) &&
-        produit.nom.toLowerCase().includes(recherche.toLowerCase())
+            (!categorieFiltre || produit.categorie === categorieFiltre) &&
+            produit.nom.toLowerCase().includes(recherche.toLowerCase())
     );
 
     return (
-        <div className="home">
+        <div className="w-full">
             <Header />
 
-            <div className="home_subtitle">
-                <p>Nos formules sont : courtes, concentrées, fabriquées en France</p>
+            <div className="bg-[var(--quaternary)] py-[15vh] px-[10vw]">
+                <p className="text-[2.5vw] text-[var(--primary)]">Nos formules sont : courtes, concentrées, fabriquées en France</p>
             </div>
 
-            <div className="home_search">
-                <h2>Nos produits</h2>
+            <div className="sticky top-[-10vh] flex flex-col gap-[2vh] justify-between py-[20vh] px-[10vw] pb-[2vw] bg-[var(--secondary)]">
+                <h2 className="text-[2.5vw] text-[var(--primary)]">Nos produits</h2>
 
-                {/*Filtres de catégorie */}
-                <div className="home_search-tags">
+                <div className="flex gap-[2vw]">
                     <button 
                         onClick={() => setCategorieFiltre(null)}
-                        className={`tag ${categorieFiltre === null ? "active" : ""}`}
+                        className={`text-[1.5vw] border border-[var(--primary)] px-[30px] py-[1vh] ${categorieFiltre === null ? "bg-[var(--primary)] text-[var(--secondary)]" : "text-[var(--primary)]"}`}
                     >
                         Tous
                     </button>
@@ -73,7 +65,7 @@ export default function Home() {
                         <button 
                             key={cat}
                             onClick={() => setCategorieFiltre(cat)}
-                            className={`tag ${categorieFiltre === cat ? "active" : ""}`}
+                            className={`text-[1.5vw] border border-[var(--primary)] px-[30px] py-[1vh] ${categorieFiltre === cat ? "bg-[var(--primary)] text-[var(--secondary)]" : "text-[var(--primary)]"}`}
                         >
                             {cat}
                         </button>
@@ -81,21 +73,20 @@ export default function Home() {
                 </div>
             </div>
 
-            {/*Liste des produits */}
-            <div className="home_produits">
+            <div className="py-[20vh] px-[10vw] bg-[var(--secondary)]">
                 {produitsFiltres.length === 0 ? (
                     <p>Aucun produit trouvé</p>
                 ) : (
-                    <div className="home_produits-container">
+                    <div className="grid grid-cols-3 gap-[5vw]">
                         {produitsFiltres.map((produit) => (
-                            <div key={produit.id} className="produit">
-                                <Link href={`/produit/${produit.id}`} className="produit_link">
-                                    <h2>{produit.nom}</h2>
-                                    <img src={produit.image} alt={produit.nom} className="produit_link-img"/>
+                            <div key={produit.id} className="flex flex-col gap-[30px] bg-white p-[30px]">
+                                <Link href={`/produit/${produit.id}`} className="flex flex-col gap-[15px]">
+                                    <h2 className="text-[var(--primary)] font-bold">{produit.nom}</h2>
+                                    <img src={produit.image} alt={produit.nom} className="w-full h-80 object-cover"/>
                                 </Link>
-                                <div className="produit_info">
-                                    <p>{produit.prix} €</p>
-                                    <button onClick={() => ajouterAuPanier({ ...produit, quantite: 1 })}>
+                                <div className="flex justify-between items-center">
+                                    <p className="text-[var(--primary)]">{produit.prix} €</p>
+                                    <button className="text-[var(--primary)] border border-[var(--primary)] px-[30px] py-[1vh]" onClick={() => ajouterAuPanier({ ...produit, quantite: 1 })}>
                                         Ajouter au panier
                                     </button>
                                 </div>
