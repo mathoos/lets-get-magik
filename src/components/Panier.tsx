ca779089547f910204eebc833c9310d379ed0fa9
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2025-02-26 11:06:21 +0100

    panier tailwind responsive

--- src/components/Panier.tsx
+++ src/components/Panier.tsx
@@ -60,12 +60,12 @@ export default function Panier({ isOpen, closePanier }: { isOpen: boolean; close
     };
 
     return (
-        <div className={`fixed top-0 right-0 w-[40vw] h-screen bg-[var(--secondary)] border-l border-[var(--primary)] transition-transform duration-300 z-50 ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
+        <div className={`fixed top-0 right-0 md:w-[40vw] w-full h-screen bg-[var(--secondary)] border-l border-[var(--primary)] transition-transform duration-300 z-50 ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
             
             {/* Nav Panier */}
-            <div className="flex items-center justify-between h-[8vh] pl-[2vw] pr-[2vw] bg-[var(--primary)] border-l border-[var(--secondary)]">
+            <div className="flex items-center justify-between h-[8vh] px-[30px] md:px-[2vw] bg-[var(--primary)] border-l border-[var(--secondary)]">
                 <h2 className="text-[var(--secondary)]">Mon Panier</h2>
-                <button className="relative w-[1.5vw] h-[1.5vw]" onClick={closePanier}>
+                <button className="relative w-[5vw] h-[5vw] md:w-[1.5vw] md:h-[1.5vw]" onClick={closePanier}>
                     <span className="absolute top-1/2 left-1/2 w-full h-[1px] bg-[var(--secondary)] transform -translate-x-1/2 -translate-y-1/2 rotate-45"></span>
                     <span className="absolute top-1/2 left-1/2 w-full h-[1px] bg-[var(--secondary)] transform -translate-x-1/2 -translate-y-1/2 -rotate-45"></span>
                 </button>
@@ -73,35 +73,37 @@ export default function Panier({ isOpen, closePanier }: { isOpen: boolean; close
 
             {/* Contenu du panier */}
             {panier.length === 0 ? (
-                <p className="text-[var(--primary)] p-[2vw]">Votre panier est vide.</p>
+                <p className="text-[var(--primary)] p-[30px] md:p-[2vw]">Votre panier est vide.</p>
             ) : (
-                <div className="flex flex-col justify-between h-[92vh] p-[2vw]">
+                <div className="flex flex-col justify-between h-[92vh] p-[30px] md:p-[2vw]">
                     
                     {/* Liste des produits */}
-                    <div className="flex flex-col gap-[1vw] overflow-y-auto">
+                    <div className="flex flex-col gap-[15px] md:gap-[1vw] overflow-y-auto">
                         {panier.map((produit) => (
-                            <div key={produit.id} className="flex justify-between items-center border-b border-[var(--primary)] pb-[1vw]">
+                            <div key={produit.id} className="flex justify-between items-center items-stretch gap-[15px] md:gap-[1vw] border-b border-[var(--primary)] pb-[15px] md:pb-[1vw]">
                                 
                                 {/* Image et Nom */}
-                                <div className="flex items-center gap-[2vw] w-full">
-                                    <img src={produit.image} alt={produit.nom} className="w-[5vw] h-[5vw] object-cover" />
+                                <div className="flex items-center gap-[1vw] w-[60vw] md:w-[70vw]">
+                                    <img src={produit.image} alt={produit.nom} className="w-[50px] h-[50px] md:w-[5vw] md:h-[5vw] object-cover" />
                                     <h2 className="text-[var(--primary)]">{produit.nom}</h2>
                                 </div>
                                 
                                 {/* Prix */}
-                                <p className="text-[var(--primary)] w-[50%] text-center">{produit.prix} €</p>
+                                <div className="flex items-center justify-center h-[100%] w-[10vw] md:w-[20vw]">
+                                    <p className="text-[3.5vw] md:text-[1vw] text-[var(--primary)">{produit.prix}€</p>
+                                </div>
 
                                 {/* Quantité & Suppression */}
-                                <div className="flex items-center gap-[2vw]">
+                                <div className="flex items-center justify-between gap-[1vw] w-[15vw] md:w-[10vw]">
                                     <input
                                         type="number"
-                                        className="no-spinner w-[2vw] h-[2vw] bg-transparent outline-none text-center appearance-none text-[var(--primary)]"
+                                        className="text-[3.5vw] md:text-[1vw] spinner w-[6vw] h-full md:w-[2vw] md:h-[2vw] bg-transparent outline-none text-center appearance-none text-[var(--primary)]"
                                         value={produit.quantite}
                                         min="1"
                                         onChange={(e) => handleChangeQuantite(produit.id, parseInt(e.target.value))}
                                     />
-                                    <button onClick={() => retirerDuPanier(produit.id)} className="w-[2vw] h-[2vw] flex items-center justify-center">
-                                        <Image src={trash} alt="Poubelle" className="w-[20px] h-[20px]" />
+                                    <button onClick={() => retirerDuPanier(produit.id)} className="w-[25px] h-[25px] md:w-[20px] md:h-[20px] flex items-center justify-center">
+                                        <Image src={trash} alt="Poubelle" className="w-[100%] h-[100%] object-contain"/>
                                     </button>
                                 </div>
                             </div>
