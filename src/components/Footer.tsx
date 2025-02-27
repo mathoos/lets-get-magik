d78dccd8f03d18191a0e0e6d6de487cbd4d41c90
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2025-02-27 08:58:47 +0100

    footer tailwind

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
-import "./Footer.scss";
-
 export default function Footer() {
     return (
-        <footer className="footer">
-            <p>© 2024 Magik. Tous droits réservés.</p>
+        <footer className="flex justify-center align-center px-[10vw] py-[5vh] bg-[var(--primary)]">
+            <p className="text-[3.5vw] md:text-[1vw] text-[var(--secondary)]">© 2025 Magik. Tous droits réservés.</p>
         </footer>
     );
 }
